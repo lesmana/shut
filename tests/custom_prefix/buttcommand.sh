c55fb2f7e6fa509137a49cc3d5ec8063c6d8d0d3
Lesmana Zimmer <lesmana@gmx.de>
2013-03-29 20:10:49 +0100

    rewrite test custom_prefix

---
+++ tests/custom_prefix/buttcommand.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+../../../butt -n prefix
--- tests/custom_prefix/sourceme.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-expectedoutput="\
-./prefix_false1.sh
-./prefix_true1.sh
-./prefix_true2.sh"
-expectedexitstatus=0
-
-buttcommand="../../butt -n prefix"
