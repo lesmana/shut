f8251b6a5e88b7287fb62f9892765a703e96d6c0
Lesmana Zimmer <lesmana@gmx.de>
2013-04-02 20:36:03 +0200

    remove sanity check

--- tests/libruntest.sh
+++ tests/libruntest.sh
@@ -3,9 +3,6 @@
 before_arrange() { :; }
 
 arrange() {
-  (test -d effect && test -d stage) ||
-        { echo "need effect and stage"; exit 1; }
-
   test -e expected && rm -rf expected
   test -e actual && rm -rf actual
 
