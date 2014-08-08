bbe5ebb098bc21eefdb6ba7a15ffd02cfa835146
Lesmana Zimmer <lesmana@gmx.de>
2014-08-08 18:27:13 +0200

    simplify test space in name

--- tests/space_in_name/runtest.sh
+++ tests/space_in_name/runtest.sh
@@ -1,11 +1,50 @@
 #! /bin/sh
 
+set -xeu
+
+shutoutput="\
+./test false 1.sh
+./test true 1.sh
+./test true 2.sh
+would run: 3
+"
+
+shutexitstatus="\
+0
+"
+
+testfalse="\
+#! /bin/sh
 set -x
+false
+"
 
-. ../libruntest.sh
+testtrue="\
+#! /bin/sh
+set -x
+true
+"
 
-shutcommand_in_actual() {
-  ../../../shut -n
-}
+rm -rf expected actual
+mkdir expected actual
 
-runtest
+(
+  cd expected
+  printf "$shutoutput" > shutoutput
+  printf "$shutexitstatus" > shutexitstatus
+)
+
+(
+  cd actual
+  printf "$testfalse" > "test false 1.sh"
+  printf "$testtrue" > "test true 1.sh"
+  printf "$testtrue" > "test true 2.sh"
+  chmod +x "test false 1.sh" "test true 1.sh" "test true 2.sh"
+  set +e
+  ../../../shut -n > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+  rm "test false 1.sh" "test true 1.sh" "test true 2.sh"
+)
+
+diff -r expected actual
