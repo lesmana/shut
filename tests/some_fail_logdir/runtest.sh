7355fc43c2c82bcfc58ece8adeb5cfbd5ca7cbfb
Lesmana Zimmer <lesmana@gmx.de>
2014-08-08 14:29:54 +0200

    simplify test some fail logdir

--- tests/some_fail_logdir/runtest.sh
+++ tests/some_fail_logdir/runtest.sh
@@ -1,11 +1,79 @@
 #! /bin/sh
 
+set -xeu
+
+testfalse="\
+#! /bin/sh
 set -x
+false
+"
 
-. ../libruntest.sh
+testfalseoutput="\
++ false
+"
 
-shutcommand_in_actual() {
-  ../../../shut -l logdir
-}
+testfalseexitstatus="\
+1
+"
 
-runtest
+testtrue="\
+#! /bin/sh
+set -x
+true
+"
+
+testtrueoutput="\
++ true
+"
+
+testtrueexitstatus="\
+0
+"
+
+shutoutput="\
+================
+./test_false1.sh
+----------------
+output:
+  + false
+----------------
+exitstatus: 1
+FAIL ./test_false1.sh
+----------------
+run: 3 pass: 2 fail: 1
+"
+
+shutexitstatus="\
+1
+"
+
+rm -rf expected actual
+mkdir expected actual
+
+(
+  cd expected
+  printf "$shutoutput" > shutoutput
+  printf "$shutexitstatus" > shutexitstatus
+  mkdir -p logdir/test_false1.sh logdir/test_true1.sh logdir/test_true2.sh
+  printf "$testfalseoutput" > logdir/test_false1.sh/output
+  printf "$testfalseexitstatus" > logdir/test_false1.sh/exitstatus
+  printf "$testtrueoutput" > logdir/test_true1.sh/output
+  printf "$testtrueexitstatus" > logdir/test_true1.sh/exitstatus
+  printf "$testtrueoutput" > logdir/test_true2.sh/output
+  printf "$testtrueexitstatus" > logdir/test_true2.sh/exitstatus
+)
+
+(
+  cd actual
+  printf "$testfalse" > test_false1.sh
+  printf "$testtrue" > test_true1.sh
+  printf "$testtrue" > test_true2.sh
+  chmod +x test_false1.sh test_true1.sh test_true2.sh
+  set +e
+  ../../../shut -l logdir > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+  rm test_false1.sh test_true1.sh test_true2.sh
+)
+
+diff -r expected actual
