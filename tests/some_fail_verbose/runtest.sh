16b43485181c4137094ce8c7eb24824fa3545c2e
Lesmana Zimmer <lesmana@gmx.de>
2014-08-08 14:32:39 +0200

    simplify test some fail verbose

--- tests/some_fail_verbose/runtest.sh
+++ tests/some_fail_verbose/runtest.sh
@@ -1,11 +1,74 @@
 #! /bin/sh
 
+set -xeu
+
+testfalse="\
+#! /bin/sh
 set -x
+false
+"
 
-. ../libruntest.sh
+testtrue="\
+#! /bin/sh
+set -x
+true
+"
 
-shutcommand_in_actual() {
-  ../../../shut -v
-}
+shutoutput="\
+================
+./test_false1.sh
+----------------
+output:
+  + false
+----------------
+exitstatus: 1
+FAIL ./test_false1.sh
+----------------
+================
+./test_true1.sh
+----------------
+output:
+  + true
+----------------
+exitstatus: 0
+PASS ./test_true1.sh
+----------------
+================
+./test_true2.sh
+----------------
+output:
+  + true
+----------------
+exitstatus: 0
+PASS ./test_true2.sh
+----------------
+run: 3 pass: 2 fail: 1
+"
 
-runtest
+shutexitstatus="\
+1
+"
+
+rm -rf expected actual
+mkdir expected actual
+
+(
+  cd expected
+  printf "$shutoutput" > shutoutput
+  printf "$shutexitstatus" > shutexitstatus
+)
+
+(
+  cd actual
+  printf "$testfalse" > test_false1.sh
+  printf "$testtrue" > test_true1.sh
+  printf "$testtrue" > test_true2.sh
+  chmod +x test_false1.sh test_true1.sh test_true2.sh
+  set +e
+  ../../../shut -v > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+  rm test_false1.sh test_true1.sh test_true2.sh
+)
+
+diff -r expected actual
