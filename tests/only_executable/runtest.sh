1e5f918b431e6814a1cf8b768870a2c682af8936
Lesmana Zimmer <lesmana@gmx.de>
2014-08-08 18:22:28 +0200

    simplify test only executable

--- tests/only_executable/runtest.sh
+++ tests/only_executable/runtest.sh
@@ -1,11 +1,56 @@
 #! /bin/sh
 
+set -xeu
+
+shutoutput="\
+================
+./test_false1.sh
+----------------
+output:
+  + false
+----------------
+exitstatus: 1
+FAIL ./test_false1.sh
+----------------
+run: 2 pass: 1 fail: 1
+"
+
+shutexitstatus="\
+1
+"
+
+testfalse="\
+#! /bin/sh
 set -x
+false
+"
 
-. ../libruntest.sh
+testtrue="\
+#! /bin/sh
+set -x
+true
+"
 
-shutcommand_in_actual() {
-  ../../../shut -n -r
-}
+rm -rf expected actual
+mkdir expected actual
 
-runtest
+(
+  cd expected
+  printf "$shutoutput" > shutoutput
+  printf "$shutexitstatus" > shutexitstatus
+)
+
+(
+  cd actual
+  printf "$testfalse" > test_false1.sh
+  printf "$testtrue" > test_true1.sh
+  printf "$testtrue" > test_true2.sh
+  chmod +x test_false1.sh test_true1.sh
+  set +e
+  ../../../shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+  rm test_false1.sh test_true1.sh test_true2.sh
+)
+
+diff -r expected actual
