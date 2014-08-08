0229c379b0255c53694263ba278700ce9a6caa8b
Lesmana Zimmer <lesmana@gmx.de>
2014-08-08 15:05:57 +0200

    add test no tests

---
+++ tests/no_tests/runtest.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+set -xeu
+
+shutoutput="\
+run: 0 pass: 0 fail: 0
+"
+
+shutexitstatus="\
+0
+"
+
+rm -rf expected actual
+mkdir expected actual
+
+(
+  cd expected
+  printf "$shutoutput" > shutoutput
+  printf "$shutexitstatus" > shutexitstatus
+)
+
+(
+  cd actual
+  set +e
+  ../../../shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
