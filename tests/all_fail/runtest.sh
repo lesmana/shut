125c254c0358e7c6d280b174709faf23879522ea
Lesmana Zimmer <lesmana@gmx.de>
2014-08-08 13:35:30 +0200

    simplify test all fail

--- tests/all_fail/runtest.sh
+++ tests/all_fail/runtest.sh
@@ -1,11 +1,60 @@
 #! /bin/sh
 
+set -xeu
+
+testfalse="\
+#! /bin/sh
 set -x
+false
+"
 
-. ../libruntest.sh
+shutoutput="\
+================
+./test_false1.sh
+----------------
+output:
+  + false
+----------------
+exitstatus: 1
+FAIL ./test_false1.sh
+----------------
+================
+./test_false2.sh
+----------------
+output:
+  + false
+----------------
+exitstatus: 1
+FAIL ./test_false2.sh
+----------------
+run: 2 pass: 0 fail: 2
+"
 
-shutcommand_in_actual() {
-  ../../../shut
-}
+shutexitstatus="\
+1
+"
 
-runtest
+rm -rf expected actual
+mkdir expected actual
+
+(
+  cd expected
+  printf "$testfalse" > test_false1.sh
+  printf "$testfalse" > test_false2.sh
+  chmod +x test_false1.sh test_false2.sh
+  printf "$shutoutput" > shutoutput
+  printf "$shutexitstatus" > shutexitstatus
+)
+
+(
+  cd actual
+  printf "$testfalse" > test_false1.sh
+  printf "$testfalse" > test_false2.sh
+  chmod +x test_false1.sh test_false2.sh
+  set +e
+  ../../../shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
