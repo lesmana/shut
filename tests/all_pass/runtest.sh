ac327fd831ac13316b9d9e344d24523143e95fb1
Lesmana Zimmer <lesmana@gmx.de>
2014-08-08 13:54:28 +0200

    simplify test all pass

--- tests/all_pass/runtest.sh
+++ tests/all_pass/runtest.sh
@@ -1,11 +1,42 @@
 #! /bin/sh
 
+set -xeu
+
+testtrue="\
+#! /bin/sh
 set -x
+true
+"
 
-. ../libruntest.sh
+shutoutput="\
+run: 2 pass: 2 fail: 0
+"
 
-shutcommand_in_actual() {
-  ../../../shut
-}
+shutexitstatus="\
+0
+"
 
-runtest
+rm -rf expected actual
+mkdir expected actual
+
+(
+  cd expected
+  printf "$testtrue" > test_true1.sh
+  printf "$testtrue" > test_true2.sh
+  chmod +x test_true1.sh test_true2.sh
+  printf "$shutoutput" > shutoutput
+  printf "$shutexitstatus" > shutexitstatus
+)
+
+(
+  cd actual
+  printf "$testtrue" > test_true1.sh
+  printf "$testtrue" > test_true2.sh
+  chmod +x test_true1.sh test_true2.sh
+  set +e
+  ../../../shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
