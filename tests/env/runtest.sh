4715a533828a87a7c13394a5a7c1d2fe47aeb0e5
Lesmana Zimmer <lesmana@gmx.de>
2014-08-10 07:09:31 +0200

    cosmetic: rename to d for shorter names

--- tests/env/runtest.sh
+++ tests/env/runtest.sh
@@ -4,37 +4,37 @@ set -xeu
 
 shutoutput="\
 ================
-./subdir1/test1
+./d1/test1
 ----------------
 output:
   $PWD/actual
-  $PWD/actual/subdir1/test1
-  $PWD/actual/subdir1/test1.dir/workdir
+  $PWD/actual/d1/test1
+  $PWD/actual/d1/test1.dir/workdir
 ----------------
 exitstatus: 0
-PASS ./subdir1/test1
+PASS ./d1/test1
 ----------------
 ================
-./subdir2/subdir21/test21
+./d2/d21/test21
 ----------------
 output:
   $PWD/actual
-  $PWD/actual/subdir2/subdir21/test21
-  $PWD/actual/subdir2/subdir21/test21.dir/workdir
+  $PWD/actual/d2/d21/test21
+  $PWD/actual/d2/d21/test21.dir/workdir
 ----------------
 exitstatus: 0
-PASS ./subdir2/subdir21/test21
+PASS ./d2/d21/test21
 ----------------
 ================
-./subdir2/test2
+./d2/test2
 ----------------
 output:
   $PWD/actual
-  $PWD/actual/subdir2/test2
-  $PWD/actual/subdir2/test2.dir/workdir
+  $PWD/actual/d2/test2
+  $PWD/actual/d2/test2.dir/workdir
 ----------------
 exitstatus: 0
-PASS ./subdir2/test2
+PASS ./d2/test2
 ----------------
 ================
 ./test0
@@ -70,22 +70,22 @@ mkdir expected actual
   cd expected
   printf "$shutoutput" > shutoutput
   printf "$shutexitstatus" > shutexitstatus
-  mkdir -p test0.dir/workdir subdir1/test1.dir/workdir subdir2/test2.dir/workdir subdir2/subdir21/test21.dir/workdir
+  mkdir -p test0.dir/workdir d1/test1.dir/workdir d2/test2.dir/workdir d2/d21/test21.dir/workdir
   printf "$testenv" > test0
-  printf "$testenv" > subdir1/test1
-  printf "$testenv" > subdir2/test2
-  printf "$testenv" > subdir2/subdir21/test21
-  chmod +x test0 subdir1/test1 subdir2/test2 subdir2/subdir21/test21
+  printf "$testenv" > d1/test1
+  printf "$testenv" > d2/test2
+  printf "$testenv" > d2/d21/test21
+  chmod +x test0 d1/test1 d2/test2 d2/d21/test21
 )
 
 (
   cd actual
-  mkdir -p subdir1 subdir2/subdir21
+  mkdir -p d1 d2/d21
   printf "$testenv" > test0
-  printf "$testenv" > subdir1/test1
-  printf "$testenv" > subdir2/test2
-  printf "$testenv" > subdir2/subdir21/test21
-  chmod +x test0 subdir1/test1 subdir2/test2 subdir2/subdir21/test21
+  printf "$testenv" > d1/test1
+  printf "$testenv" > d2/test2
+  printf "$testenv" > d2/d21/test21
+  chmod +x test0 d1/test1 d2/test2 d2/d21/test21
   set +e
   ../../../shut -r -v > shutoutput 2>&1
   printf "$?\n" > shutexitstatus
