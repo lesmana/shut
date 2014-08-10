7bb1a8c9f3d6e5e05cf2fda83408c2a2fcfe4e3c
Lesmana Zimmer <lesmana@gmx.de>
2014-08-10 05:28:44 +0200

    add test dir exists abort

---
+++ tests/dir_exists_abort/runtest.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+set -xeu
+
+shutoutput="\
+name exists: logdir
+will not overwrite
+use -f to overwrite
+"
+
+shutexitstatus="\
+1
+"
+
+rm -rf expected actual
+mkdir expected actual
+
+(
+  cd expected
+  printf "$shutoutput" > shutoutput
+  printf "$shutexitstatus" > shutexitstatus
+  mkdir logdir
+  touch logdir/existinglogdir
+)
+
+(
+  cd actual
+  mkdir logdir
+  touch logdir/existinglogdir
+  set +e
+  ../../../shut -l logdir > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
