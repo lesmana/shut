67eaa72d6afe6cdeca1f58fb96ef3e3bf7f37271
Lesmana Zimmer <lesmana@gmx.de>
2014-08-29 02:26:54 +0200

    rewrite test recursive

--- tests/test_recursive.sh
+++ tests/test_recursive.sh
@@ -2,47 +2,58 @@
 
 set -xeu
 
-shutoutput="\
+mkdir -p actual/subdir1 actual/subdir2/subdir21
+
+printf "\
+#! /bin/sh
+set -x
+true
+" > actual/test0
+
+printf "\
+#! /bin/sh
+set -x
+true
+" > actual/subdir1/test1
+
+printf "\
+#! /bin/sh
+set -x
+true
+" > actual/subdir2/test2
+
+printf "\
+#! /bin/sh
+set -x
+true
+" > actual/subdir2/subdir21/test21
+
+chmod +x \
+      actual/test0 \
+      actual/subdir1/test1 \
+      actual/subdir2/test2 \
+      actual/subdir2/subdir21/test21
+
+cp -a actual expected
+
+printf "\
 ./subdir1/test1
 ./subdir2/subdir21/test21
 ./subdir2/test2
 ./test0
 would run: 4
-"
+" > expected/shutoutput
 
-shutexitstatus="\
-0
-"
+printf "0\n" > expected/shutexitstatus
 
-testtrue="\
-#! /bin/sh
-set -x
-true
-"
-
-rm -rf expected actual
-mkdir expected actual
-
-(
-  cd expected
-  printf "$shutoutput" > shutoutput
-  printf "$shutexitstatus" > shutexitstatus
-  mkdir shutdir
-)
+mkdir expected/shutdir
 
 (
   cd actual
-  mkdir -p subdir1 subdir2/subdir21
-  printf "$testtrue" > test0
-  printf "$testtrue" > subdir1/test1
-  printf "$testtrue" > subdir2/test2
-  printf "$testtrue" > subdir2/subdir21/test21
-  chmod +x test0 subdir1/test1 subdir2/test2 subdir2/subdir21/test21
   set +e
   shut -n -r > shutoutput 2>&1
   printf "$?\n" > shutexitstatus
   set -e
-  rm -rf test0 subdir1 subdir2
 )
 
 diff -r expected actual
