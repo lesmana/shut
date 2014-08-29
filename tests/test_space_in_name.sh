026e947dcb76d80c97e9ac31503b95a48f44df4c
Lesmana Zimmer <lesmana@gmx.de>
2014-08-29 02:31:45 +0200

    rewrite test space in name

--- tests/test_space_in_name.sh
+++ tests/test_space_in_name.sh
@@ -2,50 +2,50 @@
 
 set -xeu
 
-shutoutput="\
+mkdir actual
+
+printf "\
+#! /bin/sh
+set -x
+false
+" > "actual/test false 1.sh"
+
+printf "\
+#! /bin/sh
+set -x
+true
+" > "actual/test true 1.sh"
+
+printf "\
+#! /bin/sh
+set -x
+true
+" > "actual/test true 2.sh"
+
+chmod +x \
+      "actual/test false 1.sh" \
+      "actual/test true 1.sh" \
+      "actual/test true 2.sh"
+
+cp -a actual expected
+
+printf "\
 ./test false 1.sh
 ./test true 1.sh
 ./test true 2.sh
 would run: 3
-"
+" > expected/shutoutput
 
-shutexitstatus="\
-0
-"
+printf "0\n" > expected/shutexitstatus
 
-testfalse="\
-#! /bin/sh
-set -x
-false
-"
-
-testtrue="\
-#! /bin/sh
-set -x
-true
-"
-
-rm -rf expected actual
-mkdir expected actual
-
-(
-  cd expected
-  printf "$shutoutput" > shutoutput
-  printf "$shutexitstatus" > shutexitstatus
-  mkdir shutdir
-)
+mkdir expected/shutdir
 
 (
   cd actual
-  printf "$testfalse" > "test false 1.sh"
-  printf "$testtrue" > "test true 1.sh"
-  printf "$testtrue" > "test true 2.sh"
-  chmod +x "test false 1.sh" "test true 1.sh" "test true 2.sh"
   set +e
   shut -n > shutoutput 2>&1
   printf "$?\n" > shutexitstatus
   set -e
-  rm "test false 1.sh" "test true 1.sh" "test true 2.sh"
 )
 
 diff -r expected actual
