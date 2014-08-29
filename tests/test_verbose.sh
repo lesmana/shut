6e8795613926d63a8bed68db7ab952fe8a35c879
Lesmana Zimmer <lesmana@gmx.de>
2014-08-29 02:33:37 +0200

    rewrite test verbose

--- tests/test_verbose.sh
+++ tests/test_verbose.sh
@@ -2,7 +2,31 @@
 
 set -xeu
 
-shutoutput="\
+mkdir actual
+
+printf "\
+#! /bin/sh
+set -x
+false
+" > actual/testf1.sh
+
+printf "\
+#! /bin/sh
+set -x
+true
+" > actual/testt1.sh
+
+printf "\
+#! /bin/sh
+set -x
+true
+" > actual/testt2.sh
+
+chmod +x actual/testf1.sh actual/testt1.sh actual/testt2.sh
+
+cp -a actual expected
+
+printf "\
 ================
 ./testf1.sh
 ----------------
@@ -31,69 +55,31 @@ exitstatus: 0
 PASS ./testt2.sh
 ----------------
 run: 3 pass: 2 fail: 1
-"
+" > expected/shutoutput
 
-shutexitstatus="\
-1
-"
+printf "1\n" > expected/shutexitstatus
 
-testfalse="\
-#! /bin/sh
-set -x
-false
-"
+mkdir -p expected/shutdir
 
-testfalseoutput="\
-+ false
-"
+mkdir -p \
+      expected/shutdir \
+      expected/shutdir/testf1.sh.dir/workdir \
+      expected/shutdir/testt1.sh.dir/workdir \
+      expected/shutdir/testt2.sh.dir/workdir
 
-testfalseexitstatus="\
-1
-"
-
-testtrue="\
-#! /bin/sh
-set -x
-true
-"
-
-testtrueoutput="\
-+ true
-"
-
-testtrueexitstatus="\
-0
-"
-
-rm -rf expected actual
-mkdir expected actual
-
-(
-  cd expected
-  printf "$shutoutput" > shutoutput
-  printf "$shutexitstatus" > shutexitstatus
-  mkdir -p shutdir
-  cd shutdir
-  mkdir -p testf1.sh.dir/workdir testt1.sh.dir/workdir testt2.sh.dir/workdir
-  printf "$testfalseoutput" > testf1.sh.dir/output
-  printf "$testfalseexitstatus" > testf1.sh.dir/exitstatus
-  printf "$testtrueoutput" > testt1.sh.dir/output
-  printf "$testtrueexitstatus" > testt1.sh.dir/exitstatus
-  printf "$testtrueoutput" > testt2.sh.dir/output
-  printf "$testtrueexitstatus" > testt2.sh.dir/exitstatus
-)
+printf "+ false\n"  > expected/shutdir/testf1.sh.dir/output
+printf "1\n"        > expected/shutdir/testf1.sh.dir/exitstatus
+printf "+ true\n"   > expected/shutdir/testt1.sh.dir/output
+printf "0\n"        > expected/shutdir/testt1.sh.dir/exitstatus
+printf "+ true\n"   > expected/shutdir/testt2.sh.dir/output
+printf "0\n"        > expected/shutdir/testt2.sh.dir/exitstatus
 
 (
   cd actual
-  printf "$testfalse" > testf1.sh
-  printf "$testtrue" > testt1.sh
-  printf "$testtrue" > testt2.sh
-  chmod +x testf1.sh testt1.sh testt2.sh
   set +e
   shut -v > shutoutput 2>&1
   printf "$?\n" > shutexitstatus
   set -e
-  rm testf1.sh testt1.sh testt2.sh
 )
 
 diff -r expected actual
