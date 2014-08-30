6e96af42d6808add7997138e59ea457f7af16b84
Lesmana Zimmer <lesmana@gmx.de>
2014-08-31 00:30:11 +0200

    revamp test some fail

--- tests/test_some_fail.sh
+++ tests/test_some_fail.sh
@@ -8,53 +8,72 @@ printf "\
 #! /bin/sh
 set -x
 false
-" > actual/testf1.sh
+" > actual/test0
 
 printf "\
 #! /bin/sh
 set -x
 true
-" > actual/testt1.sh
+" > actual/test1
+
+printf "\
+#! /bin/sh
+set -x
+false
+" > actual/test2
 
 printf "\
 #! /bin/sh
 set -x
 true
-" > actual/testt2.sh
+" > actual/test3
 
-chmod +x actual/testf1.sh actual/testt1.sh actual/testt2.sh
+chmod +x \
+      actual/test0 \
+      actual/test1 \
+      actual/test2 \
+      actual/test3
 
 cp -a actual expected
 
 printf "\
 ================
-./testf1.sh
+./test0
 ----------------
 output:
   + false
 ----------------
 exitstatus: 1
-FAIL ./testf1.sh
+FAIL ./test0
 ----------------
-run: 3 pass: 2 fail: 1
+================
+./test2
+----------------
+output:
+  + false
+----------------
+exitstatus: 1
+FAIL ./test2
+----------------
+run: 4 pass: 2 fail: 2
 " > expected/shutoutput
 
 printf "1\n" > expected/shutexitstatus
 
-mkdir -p expected/shutdir
-
 mkdir -p \
-      expected/shutdir \
-      expected/shutdir/testf1.sh.dir/workdir \
-      expected/shutdir/testt1.sh.dir/workdir \
-      expected/shutdir/testt2.sh.dir/workdir
+      expected/shutdir/test0.dir/workdir \
+      expected/shutdir/test1.dir/workdir \
+      expected/shutdir/test2.dir/workdir \
+      expected/shutdir/test3.dir/workdir
 
-printf "+ false\n"  > expected/shutdir/testf1.sh.dir/output
-printf "1\n"        > expected/shutdir/testf1.sh.dir/exitstatus
-printf "+ true\n"   > expected/shutdir/testt1.sh.dir/output
-printf "0\n"        > expected/shutdir/testt1.sh.dir/exitstatus
-printf "+ true\n"   > expected/shutdir/testt2.sh.dir/output
-printf "0\n"        > expected/shutdir/testt2.sh.dir/exitstatus
+printf "+ false\n"  > expected/shutdir/test0.dir/output
+printf "1\n"        > expected/shutdir/test0.dir/exitstatus
+printf "+ true\n"   > expected/shutdir/test1.dir/output
+printf "0\n"        > expected/shutdir/test1.dir/exitstatus
+printf "+ false\n"  > expected/shutdir/test2.dir/output
+printf "1\n"        > expected/shutdir/test2.dir/exitstatus
+printf "+ true\n"   > expected/shutdir/test3.dir/output
+printf "0\n"        > expected/shutdir/test3.dir/exitstatus
 
 (
   cd actual
