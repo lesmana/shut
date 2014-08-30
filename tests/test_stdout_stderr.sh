22eaa4efc2e3b84644196b5639ae7df939cc9770
Lesmana Zimmer <lesmana@gmx.de>
2014-08-30 11:06:39 +0200

    add test stdout stderr

---
+++ tests/test_stdout_stderr.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir actual
+
+printf "\
+#! /bin/sh
+echo stdout
+echo stderr >&2
+" > actual/test0.sh
+
+chmod +x actual/test0.sh
+
+cp -a actual expected
+
+printf "\
+================
+./test0.sh
+----------------
+output:
+  stdout
+  stderr
+----------------
+exitstatus: 0
+PASS ./test0.sh
+----------------
+run: 1 pass: 1 fail: 0
+" > expected/shutoutput
+
+printf "0\n" > expected/shutexitstatus
+
+mkdir -p expected/shutdir/test0.sh.dir/workdir
+
+printf "\
+stdout
+stderr
+" > expected/shutdir/test0.sh.dir/output
+
+printf "0\n" > expected/shutdir/test0.sh.dir/exitstatus
+
+(
+  cd actual
+  set +e
+  shut -v > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
