5a9cc380268d6eec38a692bc3a79443974f0b383
Lesmana Zimmer <lesmana@gmx.de>
2014-08-31 00:34:34 +0200

    revamp space in name

--- tests/test_space_in_name.sh
+++ tests/test_space_in_name.sh
@@ -2,48 +2,61 @@
 
 set -xeu
 
-mkdir actual
+mkdir -p "actual/d 1"
 
 printf "\
 #! /bin/sh
 set -x
 false
-" > "actual/test false 1.sh"
+" > "actual/test 0"
 
 printf "\
 #! /bin/sh
 set -x
 true
-" > "actual/test true 1.sh"
+" > "actual/d 1/test 1"
 
-printf "\
-#! /bin/sh
-set -x
-true
-" > "actual/test true 2.sh"
-
-chmod +x \
-      "actual/test false 1.sh" \
-      "actual/test true 1.sh" \
-      "actual/test true 2.sh"
+chmod +x "actual/test 0" "actual/d 1/test 1"
 
 cp -a actual expected
 
 printf "\
-./test false 1.sh
-./test true 1.sh
-./test true 2.sh
-would run: 3
+================
+./d 1/test 1
+----------------
+output:
+  + true
+----------------
+exitstatus: 0
+PASS ./d 1/test 1
+----------------
+================
+./test 0
+----------------
+output:
+  + false
+----------------
+exitstatus: 1
+FAIL ./test 0
+----------------
+run: 2 pass: 1 fail: 1
 " > expected/shutoutput
 
-printf "0\n" > expected/shutexitstatus
+printf "1\n" > expected/shutexitstatus
 
-mkdir expected/shutdir
+mkdir -p \
+      "expected/shutdir/test 0.dir/workdir" \
+      "expected/shutdir/d 1/test 1.dir/workdir"
+
+printf "+ false\n"  > "expected/shutdir/test 0.dir/output"
+printf "1\n"        > "expected/shutdir/test 0.dir/exitstatus"
+printf "+ true\n"   > "expected/shutdir/d 1/test 1.dir/output"
+printf "0\n"        > "expected/shutdir/d 1/test 1.dir/exitstatus"
 
 (
   cd actual
   set +e
-  shut -n > shutoutput 2>&1
+  shut -v > shutoutput 2>&1
   printf "$?\n" > shutexitstatus
   set -e
 )
