6b3995a9c57dd23017bb7765d4f3ed32efa5fb46
Lesmana Zimmer <lesmana@gmx.de>
2014-08-31 02:47:18 +0200

    revamp test dir exists overwrite

--- tests/test_dir_exists_overwrite.sh
+++ tests/test_dir_exists_overwrite.sh
@@ -7,54 +7,25 @@ mkdir -p actual actual/logdir
 printf "\
 #! /bin/sh
 set -x
-false
-" > actual/testf1.sh
-
-printf "\
-#! /bin/sh
-set -x
 true
-" > actual/testt1.sh
+" > actual/test0
 
-printf "\
-#! /bin/sh
-set -x
-true
-" > actual/testt2.sh
-
-chmod +x actual/testf1.sh actual/testt1.sh actual/testt2.sh
+chmod +x actual/test0
 
 cp -a actual expected
 
 touch actual/logdir/existinglogdir
 
 printf "\
-================
-./testf1.sh
-----------------
-output:
-  + false
-----------------
-exitstatus: 1
-FAIL ./testf1.sh
-----------------
-run: 3 pass: 2 fail: 1
+run: 1 pass: 1 fail: 0
 " > expected/shutoutput
 
-printf "1\n" > expected/shutexitstatus
+printf "0\n" > expected/shutexitstatus
 
-mkdir -p \
-      expected/logdir \
-      expected/logdir/testf1.sh.dir/workdir \
-      expected/logdir/testt1.sh.dir/workdir \
-      expected/logdir/testt2.sh.dir/workdir
+mkdir -p expected/logdir/test0.dir/workdir
 
-printf "+ false\n"  > expected/logdir/testf1.sh.dir/output
-printf "1\n"        > expected/logdir/testf1.sh.dir/exitstatus
-printf "+ true\n"   > expected/logdir/testt1.sh.dir/output
-printf "0\n"        > expected/logdir/testt1.sh.dir/exitstatus
-printf "+ true\n"   > expected/logdir/testt2.sh.dir/output
-printf "0\n"        > expected/logdir/testt2.sh.dir/exitstatus
+printf "+ true\n"   > expected/logdir/test0.dir/output
+printf "0\n"        > expected/logdir/test0.dir/exitstatus
 
 (
   cd actual
