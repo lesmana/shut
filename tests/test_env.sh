179ea0826645dbaa63f0519b1ee99d4494c37e57
Lesmana Zimmer <lesmana@gmx.de>
2014-08-31 02:18:19 +0200

    revamp test env

--- tests/test_env.sh
+++ tests/test_env.sh
@@ -2,7 +2,7 @@
 
 set -xeu
 
-mkdir -p actual/d1 actual/d2/d21
+mkdir -p actual/d1
 
 printf '\
 #! /bin/sh
@@ -18,83 +18,19 @@ echo $SHUT_TEST
 echo $SHUT_TESTPWD
 ' > actual/d1/test1
 
-printf '\
-#! /bin/sh
-echo $SHUT_PWD
-echo $SHUT_TEST
-echo $SHUT_TESTPWD
-' > actual/d2/test2
-
-printf '\
-#! /bin/sh
-echo $SHUT_PWD
-echo $SHUT_TEST
-echo $SHUT_TESTPWD
-' > actual/d2/d21/test21
-
-chmod +x \
-      actual/test0 \
-      actual/d1/test1 \
-      actual/d2/test2 \
-      actual/d2/d21/test21
+chmod +x actual/test0 actual/d1/test1
 
 cp -a actual expected
 
 printf "\
-================
-./d1/test1
-----------------
-output:
-  $PWD/actual
-  $PWD/actual/d1/test1
-  $PWD/actual/shutdir/d1/test1.dir/workdir
-----------------
-exitstatus: 0
-PASS ./d1/test1
-----------------
-================
-./d2/d21/test21
-----------------
-output:
-  $PWD/actual
-  $PWD/actual/d2/d21/test21
-  $PWD/actual/shutdir/d2/d21/test21.dir/workdir
-----------------
-exitstatus: 0
-PASS ./d2/d21/test21
-----------------
-================
-./d2/test2
-----------------
-output:
-  $PWD/actual
-  $PWD/actual/d2/test2
-  $PWD/actual/shutdir/d2/test2.dir/workdir
-----------------
-exitstatus: 0
-PASS ./d2/test2
-----------------
-================
-./test0
-----------------
-output:
-  $PWD/actual
-  $PWD/actual/test0
-  $PWD/actual/shutdir/test0.dir/workdir
-----------------
-exitstatus: 0
-PASS ./test0
-----------------
-run: 4 pass: 4 fail: 0
+run: 2 pass: 2 fail: 0
 " > expected/shutoutput
 
 printf "0\n" > expected/shutexitstatus
 
 mkdir -p \
       expected/shutdir/test0.dir/workdir \
-      expected/shutdir/d1/test1.dir/workdir \
-      expected/shutdir/d2/test2.dir/workdir \
-      expected/shutdir/d2/d21/test21.dir/workdir
+      expected/shutdir/d1/test1.dir/workdir
 
 printf "\
 $PWD/actual
@@ -108,27 +44,13 @@ $PWD/actual/d1/test1
 $PWD/actual/shutdir/d1/test1.dir/workdir
 " > expected/shutdir/d1/test1.dir/output
 
-printf "\
-$PWD/actual
-$PWD/actual/d2/test2
-$PWD/actual/shutdir/d2/test2.dir/workdir
-" > expected/shutdir/d2/test2.dir/output
-
-printf "\
-$PWD/actual
-$PWD/actual/d2/d21/test21
-$PWD/actual/shutdir/d2/d21/test21.dir/workdir
-" > expected/shutdir/d2/d21/test21.dir/output
-
 printf "0\n" > expected/shutdir/test0.dir/exitstatus
 printf "0\n" > expected/shutdir/d1/test1.dir/exitstatus
-printf "0\n" > expected/shutdir/d2/test2.dir/exitstatus
-printf "0\n" > expected/shutdir/d2/d21/test21.dir/exitstatus
 
 (
   cd actual
   set +e
-  shut -v > shutoutput 2>&1
+  shut > shutoutput 2>&1
   printf "$?\n" > shutexitstatus
   set -e
 )
