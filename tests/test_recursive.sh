f5b93ecffe2664b5f57d316378a3debf5e805e21
Lesmana Zimmer <lesmana@gmx.de>
2014-09-11 11:56:43 +0200

    add test recursive

---
+++ tests/test_recursive.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual/d1
+
+touch actual/test0 actual/d1/test1
+
+chmod +x actual/test0 actual/d1/test1
+
+cp -a actual expected
+
+printf "\
+./d1/test1
+./test0
+would run: 2
+" > expected/shutoutput
+
+printf "0\n" > expected/shutexitstatus
+
+mkdir expected/shutdir
+
+printf "\
+$SHUT_TESTPWD/actual/d1/test1
+$SHUT_TESTPWD/actual/test0
+" > expected/shutdir/tests
+
+(
+  cd actual
+  set +e
+  shut -n > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
