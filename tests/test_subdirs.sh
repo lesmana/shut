9ef64dc8d92e8504b1d7a226ab0cd09f723f3b24
Lesmana Zimmer <lesmana@gmx.de>
2014-09-14 20:51:41 +0200

    expand test subdirs

--- tests/test_subdirs.sh
+++ tests/test_subdirs.sh
@@ -2,26 +2,66 @@
 
 set -xeu
 
-mkdir -p actual/d1
+mkdir -p actual/d1 actual/d2/d3
 
-touch actual/test0 actual/d1/test1
+printf "\
+#! /bin/sh
+set -x
+false
+" > actual/d1/test1
 
-chmod +x actual/test0 actual/d1/test1
+printf "\
+#! /bin/sh
+set -x
+true
+" > actual/d2/d3/test3
+
+chmod +x actual/d1/test1 actual/d2/d3/test3
 
 cp -a actual expected
 
+mkdir -p \
+      expected/shutdir/d1/test1.dir/workdir \
+      expected/shutdir/d2/d3/test3.dir/workdir
+
+printf "+ false\n"  > expected/shutdir/d1/test1.dir/output
+printf "1\n"        > expected/shutdir/d1/test1.dir/exitstatus
+
+printf "+ true\n"   > expected/shutdir/d2/d3/test3.dir/output
+printf "0\n"        > expected/shutdir/d2/d3/test3.dir/exitstatus
+
 printf "\
 ./d1/test1
-./test0
-would run: 2
+./d2/d3/test3
+" > expected/shutdir/tests
+
+printf "\
+./d2/d3/test3
+" > expected/shutdir/pass
+
+printf "\
+./d1/test1
+" > expected/shutdir/fail
+
+printf "\
+================
+./d1/test1
+----------------
+output:
+  + false
+----------------
+exitstatus: 1
+FAIL ./d1/test1
+----------------
+run: 2 pass: 1 fail: 1
 " > expected/shutoutput
 
-printf "0\n" > expected/shutexitstatus
+printf "1\n" > expected/shutexitstatus
 
 (
   cd actual
   set +e
-  shut -n > shutoutput 2>&1
+  shut > shutoutput 2>&1
   printf "$?\n" > shutexitstatus
   set -e
 )
