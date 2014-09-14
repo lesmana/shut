1bcd57fd5340d04e0556da1cfa311052fa743384
Lesmana Zimmer <lesmana@gmx.de>
2014-09-14 18:33:53 +0200

    add test no action

---
+++ tests/test_no_action.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir actual
+
+touch actual/test0 actual/test1
+
+chmod +x actual/test0 actual/test1
+
+cp -a actual expected
+
+printf "\
+./test0
+./test1
+would run: 2
+" > expected/shutoutput
+
+printf "0\n" > expected/shutexitstatus
+
+(
+  cd actual
+  set +e
+  shut -n > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
