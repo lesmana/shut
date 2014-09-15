6be46bbba5185f8694448ddd29bc1e8468f18900
Lesmana Zimmer <lesmana@gmx.de>
2014-09-15 09:02:43 +0200

    skip shutdir

---
+++ tests/test_skip_shutdir.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual actual/shutdir
+
+touch actual/testtake actual/shutdir/testskip
+
+chmod +x actual/testtake actual/shutdir/testskip
+
+cp -a actual expected
+
+printf "\
+./testtake
+would run: 1
+" > expected/shutoutput
+
+printf "0\n" > expected/shutexitstatus
+
+(
+  cd actual
+  set +e
+  shut -n > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
