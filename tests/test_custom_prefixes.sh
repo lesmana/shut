f343527bc3a19b132385fb222f6d866c8eca80ec
Lesmana Zimmer <lesmana@gmx.de>
2014-09-18 17:21:51 +0200

    more specific test custom prefixes

--- tests/test_custom_prefixes.sh
+++ tests/test_custom_prefixes.sh
@@ -4,16 +4,18 @@ set -xeu
 
 mkdir -p actual
 
-touch actual/foo actual/bar actual/baz
+touch actual/foo1 actual/foo2 actual/bar1 actual/bar2 actual/baz1 actual/baz2
 
-chmod +x actual/foo actual/bar actual/baz
+chmod +x actual/*
 
 cp -a actual expected
 
 printf "\
-./bar
-./foo
-would run: 2
+./bar1
+./bar2
+./foo1
+./foo2
+would run: 4
 " > expected/shutoutput
 
 printf "0\n" > expected/shutexitstatus
