9f7061655db9548d181491c2916287269c70949b
Lesmana Zimmer <lesmana@gmx.de>
2014-09-18 17:23:30 +0200

    handle exact name file

---
+++ tests/test_exactname_file.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual
+
+touch actual/exactname actual/exactnameplusextra actual/othername
+
+chmod +x actual/*
+
+cp -a actual expected
+
+printf "\
+./exactname
+would run: 1
+" > expected/shutoutput
+
+printf "0\n" > expected/shutexitstatus
+
+(
+  cd actual
+  set +e
+  shut -n exactname > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
