9b11dc19eb384b1fb2ff8d5f2eee9b897c9db9ca
Lesmana Zimmer <lesmana@gmx.de>
2014-09-22 20:36:31 +0200

    test error mktemp

---
+++ tests/errors/test_mktemp.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual
+
+touch actual/test0
+
+chmod +x actual/test0
+
+cp -a actual expected
+
+printf "\
+error creating tempfile
+cannot continue
+" > expected/shutoutput
+
+printf "2\n" > expected/shutexitstatus
+
+printf '\
+#! /bin/sh
+exit 13
+' > mktemp
+
+chmod +x mktemp
+
+(
+  cd actual
+  set +e
+  PATH=$SHUT_TESTPWD:$PATH
+  shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
