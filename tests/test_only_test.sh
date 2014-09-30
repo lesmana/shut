c1fd0eb4b0c213e51fef9c5abc9c0eda0858d85d
Lesmana Zimmer <lesmana@gmx.de>
2014-09-30 18:21:49 +0200

    fix find argument bug: -path to -regex

--- tests/test_only_test.sh
+++ tests/test_only_test.sh
@@ -2,18 +2,40 @@
 
 set -xeu
 
-mkdir -p actual
+mkdir -p actual/testdir actual/xtestdir
 
-touch actual/test0 actual/test1 actual/nottest
+touch \
+      actual/test0 \
+      actual/test1 \
+      actual/nottest \
+      actual/testdir/test0 \
+      actual/testdir/test1 \
+      actual/testdir/nottest \
+      actual/xtestdir/test0 \
+      actual/xtestdir/test1 \
+      actual/xtestdir/nottest
 
-chmod +x actual/test0 actual/test1 actual/nottest
+chmod +x \
+      actual/test0 \
+      actual/test1 \
+      actual/nottest \
+      actual/testdir/test0 \
+      actual/testdir/test1 \
+      actual/testdir/nottest \
+      actual/xtestdir/test0 \
+      actual/xtestdir/test1 \
+      actual/xtestdir/nottest
 
 cp -a actual expected
 
 printf "\
 ./test0
 ./test1
-would run: 2
+./testdir/test0
+./testdir/test1
+./xtestdir/test0
+./xtestdir/test1
+would run: 6
 " > expected/shutoutput
 
 printf "0\n" > expected/shutexitstatus
