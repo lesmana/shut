101b94463ae3606cf7538611e3bf1e728b134654
Lesmana Zimmer <lesmana@gmx.de>
2014-10-01 18:26:54 +0200

    handle error cd shutdir

---
+++ tests/errors/test_cd_shutdir.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual
+
+touch actual/test0
+
+chmod +x actual/test0
+
+cp -a actual expected
+
+printf "\
+error changing directory to $SHUT_TESTPWD/actual/shutdir
+cannot continue
+" > expected/shutoutput
+
+printf "3\n" > expected/shutexitstatus
+
+printf "\
+#! /bin/sh
+if [ \"\$*\" = \"tests $SHUT_TESTPWD/actual/shutdir\" ]; then
+  rm -rf $SHUT_TESTPWD/actual/shutdir
+else
+  PATH=$PATH cp \"\$@\"
+fi
+" > cp
+
+chmod +x cp
+
+(
+  cd actual
+  set +e
+  PATH=$SHUT_TESTPWD:$PATH
+  shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
