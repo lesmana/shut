c6750e739ed1b6d415859d43047f99b290920402
Lesmana Zimmer <lesmana@gmx.de>
2014-10-01 18:12:17 +0200

    handle error cp tests shutdir

---
+++ tests/errors/test_cp_tests_shutdir.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual
+
+touch actual/test0
+
+chmod +x actual/test0
+
+cp -a actual expected
+
+mkdir -p expected/shutdir
+
+printf "\
+error copying tests to $SHUT_TESTPWD/actual/shutdir
+cannot continue
+" > expected/shutoutput
+
+printf "3\n" > expected/shutexitstatus
+
+printf "\
+#! /bin/sh
+if [ \"\$*\" = \"tests $SHUT_TESTPWD/actual/shutdir\" ]; then
+  exit 23
+else
+  PATH=$PATH cp \"\$@\"
+fi
+" > cp
+
+chmod +x cp
+
+(
+  cd actual
+  set +e
+  PATH=$SHUT_TESTPWD:$PATH
+  shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
