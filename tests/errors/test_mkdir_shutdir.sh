01b38002b3b30f21065d59ea0c106ea8e586dbb7
Lesmana Zimmer <lesmana@gmx.de>
2014-10-01 18:08:42 +0200

    handle error mkdir shutdir

---
+++ tests/errors/test_mkdir_shutdir.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual
+
+touch actual/test0
+
+chmod +x actual/test0
+
+cp -a actual expected
+
+printf "\
+error creating directory $SHUT_TESTPWD/actual/shutdir
+cannot continue
+" > expected/shutoutput
+
+printf "3\n" > expected/shutexitstatus
+
+printf "\
+#! /bin/sh
+if [ \"\$*\" = \"-p $SHUT_TESTPWD/actual/shutdir\" ]; then
+  exit 23
+else
+  PATH=$PATH mkdir \"\$@\"
+fi
+" > mkdir
+
+chmod +x mkdir
+
+(
+  cd actual
+  set +e
+  PATH=$SHUT_TESTPWD:$PATH
+  shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
