5761ee835659b7fb4546a4f3f2c0337ba7a57c30
Lesmana Zimmer <lesmana@gmx.de>
2014-10-01 16:45:27 +0200

    fix close stdin and add test close stdin

---
+++ tests/test_close_stdin.sh
@@ -0,0 +1,68 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual
+
+printf '\
+#! /bin/sh
+if read line; then
+  echo "got line: $line"
+else
+  echo "got nothing"
+fi
+' > actual/test0
+
+printf '\
+#! /bin/sh
+echo "yay"
+' > actual/test1
+
+chmod +x actual/test0 actual/test1
+
+cp -a actual expected
+
+mkdir -p \
+      expected/shutdir/test0.dir/workdir \
+      expected/shutdir/test1.dir/workdir
+
+printf "\
+got nothing
+" > expected/shutdir/test0.dir/output
+
+printf "0\n" > expected/shutdir/test0.dir/exitstatus
+
+printf "\
+yay
+" > expected/shutdir/test1.dir/output
+
+printf "0\n" > expected/shutdir/test1.dir/exitstatus
+
+printf "\
+./test0
+./test1
+" > expected/shutdir/tests
+
+printf "\
+./test0
+./test1
+" > expected/shutdir/pass
+
+printf "\
+" > expected/shutdir/fail
+
+printf "\
+run: 2 pass: 2 fail: 0
+" > expected/shutoutput
+
+printf "0\n" > expected/shutexitstatus
+
+(
+  cd actual
+  set +e
+  shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
