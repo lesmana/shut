8fce6146a7ca7d5a1dd83a518c2edb4242088ced
Lesmana Zimmer <lesmana@gmx.de>
2014-10-03 21:20:26 +0200

    handle error cd workdir

---
+++ tests/errors/test_cd_workdir.sh
@@ -0,0 +1,56 @@
+#! /bin/sh
+
+set -xeu
+
+mkdir -p actual
+
+touch actual/test0
+
+chmod +x actual/test0
+
+cp -a actual expected
+
+mkdir -p expected/shutdir/test0.dir
+
+printf "\
+error changing directory to workdir
+failed tests:
+./test0
+----------------
+run: 1 pass: 0 fail: 1
+" > expected/shutoutput
+
+printf "1\n" > expected/shutexitstatus
+
+printf "\
+./test0
+" > expected/shutdir/tests
+
+printf "\
+" > expected/shutdir/pass
+
+printf "\
+./test0
+" > expected/shutdir/fail
+
+printf "\
+#! /bin/sh
+if [ \"\$*\" = \"-p workdir\" ]; then
+  :
+else
+  PATH=$PATH mkdir \"\$@\"
+fi
+" > mkdir
+
+chmod +x mkdir
+
+(
+  cd actual
+  set +e
+  PATH=$SHUT_TESTPWD:$PATH
+  shut > shutoutput 2>&1
+  printf "$?\n" > shutexitstatus
+  set -e
+)
+
+diff -r expected actual
