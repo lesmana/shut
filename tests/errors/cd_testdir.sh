3586a082c78942ff8f74c04a79a93e4d1c976b9c
Lesmana Zimmer <lesmana@gmx.de>
2014-10-28 03:43:31 +0100

    test error handling in runtestintestdir

---
+++ tests/errors/cd_testdir.sh
@@ -0,0 +1,70 @@
+#! /bin/sh
+
+set -xeu
+
+# prepare actual
+
+mkdir -p actual
+
+touch actual/test0
+
+chmod +x actual/test0
+
+# prepare expected
+
+cp -a actual expected
+
+mkdir -p expected/shutdir
+
+# prepare shutdir
+
+printf -- "\
+./test0
+" > expected/shutdir/testsfound
+
+printf -- "\
+" > expected/shutdir/testsrun
+
+printf -- "\
+" > expected/shutdir/testspass
+
+printf -- "\
+" > expected/shutdir/testsfail
+
+# prepare shut output
+
+printf -- "\
+" > expected/stdout
+
+printf -- "\
+error changing directory to testdir: test000001
+" > expected/stderr
+
+printf -- "\
+3
+" > expected/exitstatus
+
+# inject error
+
+printf -- '#! /bin/sh
+realmkdir="%s"
+if [ "$*" != "-- test000001" ]; then
+  "$realmkdir" "$@"
+fi
+' "$(which mkdir)" > mkdir
+
+chmod +x mkdir
+
+export PATH="$PWD:$PATH"
+
+# run shut
+
+(
+  cd actual
+  shut > stdout 2> stderr
+  printf -- "$?\n" > exitstatus
+) || true
+
+# compare
+
+diff -r expected actual
---
+++ tests/errors/mkdir_testdir.sh
@@ -0,0 +1,76 @@
+#! /bin/sh
+
+set -xeu
+
+# prepare actual
+
+mkdir -p actual
+
+touch actual/test0
+
+chmod +x actual/test0
+
+# prepare expected
+
+cp -a actual expected
+
+mkdir -p expected/shutdir
+
+# prepare shutdir
+
+printf -- "\
+./test0
+" > expected/shutdir/testsfound
+
+printf -- "\
+" > expected/shutdir/testsrun
+
+printf -- "\
+" > expected/shutdir/testspass
+
+printf -- "\
+" > expected/shutdir/testsfail
+
+# prepare shut output
+
+printf -- "\
+" > expected/stdout
+
+printf -- "\
+error creating testdir: test000001
+" > expected/stderr
+
+printf -- "\
+3
+" > expected/exitstatus
+
+# inject error
+
+printf -- '#! /bin/sh
+realmkdir="%s"
+if [ "$*" = "-- test000001" ]; then
+  touch "test000001"
+  "$realmkdir" "$@"
+  exitstatus=$?
+  rm "test000001"
+  exit $exitstatus
+else
+  "$realmkdir" "$@"
+fi
+' "$(which mkdir)" > mkdir
+
+chmod +x mkdir
+
+export PATH="$PWD:$PATH"
+
+# run shut
+
+(
+  cd actual
+  shut > stdout 2> stderr
+  printf -- "$?\n" > exitstatus
+) || true
+
+# compare
+
+diff -r expected actual
