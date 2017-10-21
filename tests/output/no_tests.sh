3bcb82fe4fffffba7de119540c4bcb1e9d7b07e9
Lesmana Zimmer <lesmana@gmx.de>
2017-10-21 08:42:40 +0200

    remove special case for no test found

--- tests/output/no_tests.sh
+++ tests/output/no_tests.sh
@@ -2,12 +2,25 @@
 
 # prepare actual
 
-mkdir -p actual expected
+mkdir -p actual
+
+# prepare expected
+
+cp -a actual expected
+
+mkdir -p expected/shutdir
+
+touch expected/shutdir/testsfound
+touch expected/shutdir/testsrun
+touch expected/shutdir/testspass
+touch expected/shutdir/testsfail
+touch expected/shutdir/testserror
 
 # prepare shut output
 
 printf -- "\
-no tests found
+================
+found: 0 run: 0 pass: 0 fail: 0
 " > expected/stdout
 
 printf -- "\
